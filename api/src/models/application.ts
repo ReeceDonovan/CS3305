a0a220260cec32029749dca9a729b215058d426d
Thomas Daniel Galligan <thomas007g@gmail.com>
2022-01-17 16:03:19 +0000

    added user and application models, added basic structure of handlers + router

---
+++ api/src/models/application.ts
@@ -0,0 +1,60 @@
+import faker = require("@faker-js/faker");
+import { Entity, PrimaryColumn, Column } from "typeorm";
+import User from "./user";
+
+export default class Application {
+  @PrimaryColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  school: string;
+
+  @Column()
+  course: string;
+
+  @Column()
+  supervisors: User[] = [];
+
+  constructor(
+    id: number,
+    name: string,
+    description: string,
+    school: string,
+    course: string,
+    supervisors: User[]
+  ) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.school = school;
+    this.course = course;
+    this.supervisors = supervisors;
+  }
+
+  getFromId(this: Application): Application {
+    return new Application(
+      this.id,
+      faker.name.findName(),
+      faker.lorem.sentences(2),
+      faker.lorem.word(),
+      faker.lorem.word(),
+      [User.create(faker.internet.email())]
+    );
+  }
+
+  update(this: Application): Application {
+    // update user to database
+    return this;
+  }
+
+  delete(this: Application): Application {
+    // delete user from database
+    return this;
+  }
+}
---
+++ api/src/router/routes.ts
@@ -0,0 +1,7 @@
+interface route {
+  path: string;
+  component: any;
+  exact?: boolean;
+}
+
+export const routes = [];
