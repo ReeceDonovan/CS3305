566a79630c48dee77c6caa7b49ed9c783c3b1611
max <sebastian.8295.8295@gmail.com>
2022-01-18 13:36:18 +0000

    email handler done

--- api/src/handlers/email.ts
+++ api/src/handlers/email.ts
@@ -3,6 +3,17 @@ import config from '../config/config'
 
 const cfg = new config()
 
-export default function handler() {
-    nodemailer.createTransport()
+export default function handler(recepient: string, subject: string, body: string) {
+    const emailProvider = cfg.get().emailConfigs[cfg.get().emailType]
+    const transportPayload = Object.assign(emailProvider, {user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS})
+    const transporter = nodemailer.createTransport(transportPayload)
+    const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: recepient,
+        subject: subject,
+        text: body
+    }
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err) console.error(`Failed to send email: ${err}\nDetails: ${mailOptions}\nInfo: ${info}`)
+    })
 }
