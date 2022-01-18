0fd59d2f66d699c3a06442a9b01e0d16b0361809
Thomas Daniel Galligan <thomas007g@gmail.com>
2022-01-18 15:52:49 +0000

    fixed header margin, simplified account form, cleaned up tsconfig

--- ui/pages/account.tsx
+++ ui/pages/account.tsx
@@ -1,70 +1,50 @@
-import type { NextPage } from 'next'
-import { Button, Form, TextInput} from 'carbon-components-react'
-import React, {useState} from 'react'
-
+import type { NextPage } from "next";
+import { Button, Form, TextInput } from "carbon-components-react";
+import React, { useState } from "react";
 
 const AccountPage: NextPage = () => {
+  const [name, setName] = useState("");
+  const [jobTitle, setJobTitle] = useState("");
+  const [school, setSchool] = useState("");
 
-  const user = getUser();
-
-
-  const [modiflag, setModiflag] = useState(true);
-
-  let name_ref = React.createRef<HTMLInputElement>();
-  let bio_ref = React.createRef<HTMLInputElement>();
-  let school_ref = React.createRef<HTMLInputElement>();
-  
-
-  const checkModified = () => {
-    if (name_ref?.current?.value != user.name || bio_ref?.current?.value != user.bio || school_ref?.current?.value != user.school){
-        setModiflag(false);
-    }
-  }
-
-  return(
+  return (
     <>
-      <Form>
-          <TextInput
-            id="name"
-            labelText="Name"
-            placeholder={user.name||"Bob Joe Name"}
-            onBlur={checkModified}
-            ref={name_ref}
-          />
+      <Form
+        style={{
+          height: "90vh",
+          width: "100%",
+        }}
+      >
+        <TextInput
+          id="name"
+          labelText="Name"
+          placeholder="Name"
+          onChange={(e) => setName(e.target.value)}
+        />
 
-          <TextInput
-            id="bio"
-            labelText="Job Title"
-            placeholder={user.bio||"Cool Lecturer and Researcher"}
-            onBlur={checkModified}
-            ref={bio_ref}
-          />
+        <TextInput
+          id="bio"
+          labelText="Job Title"
+          placeholder="Job Title"
+          onChange={(e) => setJobTitle(e.target.value)}
+        />
 
-          <TextInput
-            id="school"
-            labelText="School"
-            placeholder={user.school||"School of Rock"}
-            onBlur={checkModified}
-            ref={school_ref}
-          />
+        <TextInput
+          id="school"
+          labelText="School"
+          placeholder="School"
+          onChange={(e) => setSchool(e.target.value)}
+        />
 
-          <Button 
-            type="submit"
-            disabled={modiflag}
-          >
-            Submit
-          </Button>
+        <Button
+          type="submit"
+          disabled={name === "" || jobTitle === "" || school === ""}
+        >
+          Submit
+        </Button>
       </Form>
     </>
-  )
-}
+  );
+};
 
-export default AccountPage
-
-const getUser = () => {
-    return {
-        name: "name",
-        bio: null,
-        school: null
-    }
-}
+export default AccountPage;
