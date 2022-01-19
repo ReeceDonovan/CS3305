4a20aaf9e6b4bf1d6de98a6147a5646e0605aaa4
Thomas Daniel Galligan <thomas007g@gmail.com>
2022-01-19 00:03:11 +0000

    formatting + better use of config

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,12 +2,16 @@ import express, { Router } from "express";
 
 import userRouter from "./router/user";
 import loginRouter from "./router/login";
+import { Config } from "./config/config";
 
 const PORT = 8000;
 
 const app = express();
 
+new Config();
+
 app.use(userRouter);
+app.use(loginRouter);
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
--- api/src/config/config.ts
+++ api/src/config/config.ts
@@ -1,88 +1,103 @@
-import * as fs from 'fs'
+import * as fs from "fs";
+import { ExitStatus } from "typescript";
 
 // Add onto this as needed
 export var config: configInterface = {
-    emailProvider: 'gmail',
-    emailConfigs: [
-        {
-            host: 'smtp.gmail.com',
-            port: 465,
-            secure: true
-        },
-        {
-            host: "smtp-mail.outlook.com",
-            secure: false,
-            port: 587,
-            tls: {
-               ciphers:'SSLv3'
-            }
-        }
-    ],
-    oauthConfig: {
-        oauthClientId: "",
-        oauthClientSecret: "",
-    }
-}
+  emailProvider: "gmail",
+  emailConfigs: [
+    {
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+    },
+    {
+      host: "smtp-mail.outlook.com",
+      secure: false,
+      port: 587,
+      tls: {
+        ciphers: "SSLv3",
+      },
+    },
+  ],
+  oauthConfig: {
+    oauthClientId: "",
+    oauthClientSecret: "",
+  },
+};
 
 interface configInterface {
-    emailProvider: string;
-    emailConfigs: Array<emailConfig>;
-    oauthConfig: oauthConfig;
+  emailProvider: string;
+  emailConfigs: Array<emailConfig>;
+  oauthConfig: oauthConfig;
 }
 
 interface emailConfig {
-    host: string;
-    port: number;
-    secure?: boolean;
-    tls?: {
-        ciphers?: string;
-    }
+  host: string;
+  port: number;
+  secure?: boolean;
+  tls?: {
+    ciphers?: string;
+  };
 }
 
 interface oauthConfig {
-    oauthClientId: string;
-    oauthClientSecret: string;
+  oauthClientId: string;
+  oauthClientSecret: string;
 }
 
 export class Config {
-    path: string = 'config.json'
-    currentConfig: configInterface;
-    
-    constructor() {
-        fs.readFile(this.path, 'utf-8', (err, data) => {
-            if (err) {
-                this.update(config)
-                this.currentConfig = config
-                return console.log(err)
-            } 
-            try {
-                this.currentConfig = JSON.parse(data);
-            } catch (error) { 
-                fs.rename(this.path, "old." + this.path, () => {
-                    console.error("Failed to rename")
-                })
-                this.update(config)
-                this.currentConfig = config
-            }
-        })
-    }
+  path: string = "config.json";
+  currentConfig: configInterface;
 
-    public update(newJSON: configInterface) {
-        fs.writeFile(this.path, JSON.stringify(newJSON), {flag: "w+"}, (err) => {
-            if (err) return console.log(err)
-            this.currentConfig = newJSON
-        })
-    }
+  constructor() {
+    try {
+      fs.readFile(this.path, "utf-8", (_err, data) => {
+        try {
+          config = JSON.parse(data);
+          console.log("Loaded config from fs", config.oauthConfig.oauthClientId)
+        } catch (e) {
+          console.error("config file corrupted");
 
-    public get(): configInterface {
-        return this.currentConfig
+          fs.rename(this.path, "old." + this.path, () => {
+            console.error("Failed to rename");
+            return;
+          });
+        }
+      });
+    } catch (e) {
+      this.update(config);
+      this.currentConfig = config;
+      console.error(e);
     }
+  }
+  //         try {
+  //             this.currentConfig = JSON.parse(data);
+  //         } catch (error) {
+  //             fs.rename(this.path, "old." + this.path, () => {
+  //                 console.error("Failed to rename")
+  //             })
+  //             this.update(config)
+  //             this.currentConfig = config
+  //         }
+  //     })
+  // }
 
-    public read(): configInterface {
-        fs.readFile(this.path, 'utf-8', (err, data) => {
-            if (err) return console.log(err)
-            this.currentConfig = JSON.parse(data);
-        })
-        return this.currentConfig
-    }
+  public update(newJSON: configInterface) {
+    fs.writeFile(this.path, JSON.stringify(newJSON), { flag: "w+" }, (err) => {
+      if (err) return console.log(err);
+      this.currentConfig = newJSON;
+    });
+  }
+
+  public get(): configInterface {
+    return this.currentConfig;
+  }
+
+  public read(): configInterface {
+    fs.readFile(this.path, "utf-8", (err, data) => {
+      if (err) return console.log(err);
+      this.currentConfig = JSON.parse(data);
+    });
+    return this.currentConfig;
+  }
 }
