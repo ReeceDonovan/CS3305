db6bb7faf6de5e85f742c2ea3518495522c717dd
sh-angl <40544709+sh-angl@users.noreply.github.com>
2022-01-19 17:16:07 +0000

    added very simple cors solving if you get stuck like not smart me

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,16 +1,27 @@
 import express from "express";
+import cors from "cors";
 
 import userRouter from "./router/user";
 import loginRouter from "./router/login";
 import pageRouter from "./router/landingPage";
+import submitRouter from "./router/submit_form";
 
 const PORT = 8000;
 
 const app = express();
 
+var corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+//use cors as a handler before being added to your router, e.g.
+// app.use(cors(corsOptions), submitRouter)
+
 app.use(userRouter);
 app.use(loginRouter);
 app.use(pageRouter);
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
