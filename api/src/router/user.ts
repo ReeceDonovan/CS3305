14195ee607495622f5da0d73b51245e08f384748
Reece Donovan <reecedonovan99@gmail.com>
2022-01-20 14:43:32 +0000

    Fixed afew errors as a result of editing the user model in previous commit
    Signed-off-by: Reece Donovan <reecedonovan99@gmail.com>

--- api/src/router/user.ts
+++ api/src/router/user.ts
@@ -4,9 +4,7 @@ import User from "../models/user";
 const userRouter = express.Router();
 
 userRouter.get("/users/:id", (req, res) => {
-    var user = new User()
-    user.id = parseInt(req.params.id)
-    res.send(user.getFromId())
-})
+  res.send(User.getById(parseInt(req.params.id)));
+});
 
-export default userRouter
+export default userRouter;
