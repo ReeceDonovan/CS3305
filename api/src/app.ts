245fd804d4580bcbc952c5f14040a2fc96c4c9d4
Thomas Daniel Galligan <thomas007g@gmail.com>
2022-01-22 15:45:23 +0000

    added jwt auth with middleware to check for authorization

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,9 +1,13 @@
 import express from "express";
 
 import { createConn } from "./models/database";
+import { decodeToken } from "./auth/tokens";
+
 import pageRouter from "./router/landingPage";
 import loginRouter from "./router/login";
 import userRouter from "./router/user";
+import User from "./models/user";
+import response from "./utils/response";
 
 const PORT = 8000;
 
@@ -14,6 +18,36 @@ const app = express();
 app.use(userRouter);
 app.use(loginRouter);
 app.use(pageRouter);
+
+const unAuthenticatedRoutes: string[] = ["/login", "/login/callback", "/about"];
+
+// middleware to check for Authorization header, to get token.
+// gets user from id in token, then sends user object to next handler
+app.use(async (req: express.Request, res: express.Response, next) => {
+
+  // if authorization is not necessary for a route, skip this middleware
+  if (req.url in unAuthenticatedRoutes) {
+    return next();
+  }
+
+  const authHeader = req.headers["authorization"];
+  if (authHeader && authHeader.split(" ")[0] == "Bearer" && authHeader.split(" ")[1]) {
+    const token = await decodeToken(authHeader.split(" ")[1]);
+    if (token !== null) {
+      const user = await User.getById(token.userID);
+      res.locals.user = user;
+      return next();
+    }
+  }
+  
+  const unauthorizedResponse: response = {
+    data: null,
+    message: "Unauthorized",
+    status: 401,
+  }
+
+  return res.status(401).json(unauthorizedResponse);
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
---
+++ api/src/auth/tokens.ts
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import config from "../config/config";
+
+export const decodeToken = async (token: string): Promise<{userID: number, exp: number} | null> => {
+    return jwt.verify(token, config.get().signingKey, (err, decoded: { userID: number; exp: number; }) => {
+        if (err) {
+            return null;
+        }
+        return decoded;
+    });
+}
+export const createToken = async (userID: number): Promise<string> => {
+  let expiry = new Date();
+  expiry.setHours(expiry.getHours() + 1);
+  jwt.sign(
+    {
+      userID,
+      exp: (Date.now() / 1000) + (60 * 60 * 24),// 1 day
+    },
+    config.get().signingKey,
+    (err: Error, token: string) => {
+        if (err) {
+            console.error(err);
+            return "";
+        }
+        return token;
+    }
+  );
+  return "";
+};
