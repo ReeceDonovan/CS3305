fdb5c36c93e3b9e615a5f5bbf80c3e6adb8ff705
ReeceDonovan <reecedonovan99@gmail.com>
2022-02-07 11:22:13 +0000

    Refactored application router endpoints (Unfinished)
    Signed-off-by: ReeceDonovan <reecedonovan99@gmail.com>

--- api/src/router/application.ts
+++ api/src/router/application.ts
@@ -3,169 +3,161 @@ import express from "express";
 import fs from "fs";
 import multer from "multer";
 import path from "path";
-import { getRepository } from "typeorm";
+import UsersApplications, { RoleType } from "../models/usersApplications";
+import { getRepository, In, Repository } from "typeorm";
 import Application from "../models/application";
-import Review, { ReviewStatus } from "../models/review";
-import User, { UserType} from "../models/user";
-import Response, { sample_401_res, sample_404_res } from "../utils/response";
-
+import Review from "../models/review";
+import User, { UserType } from "../models/user";
+import Response from "../utils/response";
+import { protectedRoute } from "../middleware/protected-route";
+import { NotFoundError, NotAuthorizedError } from "../errors";
 
 const upload = multer({ storage: multer.memoryStorage() });
 const appRouter = express.Router();
+appRouter.use(protectedRoute);
 
+const check_access_app = async (application: Application, user: User) => {
+  if (user.role === UserType.COORDINATOR) {
+    return true;
+  }
 
-const check_access_app = (application: Application, user: User) => {
-  return application.submitter === user || application.supervisors.includes(user) || application.coauthors.includes(user) || application.reviewers.includes(user) || user.role === "COORDINATOR"
-}
+  const userApplicationRepo: Repository<UsersApplications> =
+    getRepository(UsersApplications);
 
-appRouter.get("/", async (req: express.Request, res: express.Response) => {
-  const target = req.query.t;
-  let applications: Array<Application>;
-  let response: Response;
+  const userApplication = await userApplicationRepo.findOne({
+    application,
+    user,
+  });
 
-  // depending on target and user type, return different applications
-  switch (target) {
-    case "review":
-      if (req.user.role == UserType.REVIEWER) {
-        applications = await Application.find({
-          relations: ["submitter", "reviews", "reviewers"],
-          where: {
-            status: ReviewStatus.INREVIEW,
-          },
-        });
+  return userApplication !== undefined;
+};
 
-        applications.forEach((application) => {
-          // filter out applications that the user is not assigned to review
-          if (!application.reviewers.includes(req.user)) {
-            applications.splice(applications.indexOf(application), 1);
-          }
-        });
+appRouter.get(
+  "/",
+  async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    const target = req.query.t;
+    const user = res.locals.user;
+    let response: Response;
+    try {
+      switch (target) {
+        case "all": {
+          if (user.role !== UserType.COORDINATOR)
+            throw new NotAuthorizedError();
+          const applications = await getRepository(Application).find();
+          response = {
+            status: 200,
+            message: "Successfully retrieved all applications",
+            data: applications,
+          };
+          break;
+        }
+        case "review": {
+          const userApplication = await getRepository(UsersApplications).find({
+            where: {
+              user,
+              role: RoleType.REVIEWER,
+            },
+            relations: ["application"],
+          });
 
-        response = {
-          status: 200,
-          message: "Successfully retrieved applications.",
-          data: applications,
-        };
-      } else {
-        response = {
-          status: 403,
-          message: "You are not authorized to view this page.",
-          data: null,
-        };
+          const applications = userApplication.map((u) => u.application);
+
+          response = {
+            status: 200,
+            message: "Successfully retrieved all applications",
+            data: applications,
+          };
+          break;
+        }
+
+        default: {
+          const userApplications = await getRepository(UsersApplications).find({
+            where: {
+              user,
+            },
+            relations: ["application"],
+          });
+
+          const applications = userApplications.map((u) => u.application);
+
+          response = {
+            status: 200,
+            message: "Successfully retrieved all applications",
+            data: applications,
+          };
+          break;
+        }
       }
-      break;
+      res.json(response);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
-    case "all":
-      if (req.user.role == UserType.COORDINATOR) {
-        applications = await Application.find();
-        response = {
-          message: "Success",
-          status: 200,
-          data: applications,
-        };
-      } else {
-        response = {
-          status: 403,
-          message: "You are not authorized to view this page.",
-          data: null,
-        };
-      }
-      break;
+appRouter.get(
+  "/:id",
+  async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    const user = res.locals.user;
+    const applicationId = req.params.id;
 
-    default:
-      applications = await Application.find({
-        relations: ["submitter", "reviews", "reviewers"],
-        where: {
-          submitter: req.user,
-        },
-      });
-      response = {
-        message: "Success",
+    try {
+      const application = await Application.findOne(applicationId);
+
+      if (!application) throw new NotFoundError();
+
+      if (!check_access_app(application, user)) throw new NotAuthorizedError();
+
+      res.json({
         status: 200,
-        data: applications,
-      };
-  }
-
-  res.json(response);
-});
-
-appRouter.get("/:id", async (req: express.Request, res: express.Response) => {
-  const application = await Application.getById(parseInt(req.params.id), [
-    "submitter",
-    "reviews",
-    "reviews.reviewer",
-    "supervisors",
-  ]);
-  
-  if (!application) {
-    return res.status(404).json(sample_404_res);
-  }
-  if (!check_access_app(application, req.user)){
-    return res.status(401).json(sample_401_res);
-  }
-
-  const OkResponse: Response = {
-    status: 200,
-    message: "Success",
-    data: application,
-  };
-
-  const UnauthorizedResponse: Response = {
-    status: 403,
-    message: "Forbidden",
-    data: null,
-  };
-
-  if (req.user.role === UserType.COORDINATOR) {
-    if (!application) {
-      const re: Response = {
-        status: 404,
-        message: "Application not found",
-        data: null,
-      };
-      return res.send(UnauthorizedResponse);
-    }
-
-    return res.json(OkResponse);
-  } else if (req.user.role === UserType.REVIEWER) {
-    if (!application.reviewers.includes(req.user)) {
-      return res.json(UnauthorizedResponse);
-    }
-  } else {
-    if (
-      application.submitter.id === req.user.id ||
-      application.supervisors.includes(req.user) ||
-      application.coauthors.includes(req.user)
-    ) {
-      return res.json(OkResponse);
+        message: "Successfully retrieved application",
+        data: application,
+      });
+    } catch (err) {
+      next(err);
     }
   }
-
-  return res.json(UnauthorizedResponse);
-});
+);
 
 appRouter.get(
   "/:id/form",
-  async (req: express.Request, res: express.Response) => {
-    const application = await Application.getById(parseInt(req.params.id));
-    if (!application) {
-      return res.status(404).json(sample_404_res);
-    }
-    if (!check_access_app(application, req.user)){
-      return res.status(401).json(sample_401_res);
-    }
-    const files = fs.readdirSync(
-      path.join(
+  async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    const user = res.locals.user;
+    const applicationId = req.params.id;
+
+    try {
+      const application = await Application.findOne(applicationId);
+
+      if (!application) throw new NotFoundError();
+
+      if (!check_access_app(application, user)) throw new NotAuthorizedError();
+
+      const files = fs.readdirSync(
         path.join(__dirname, `../../../data/pdf_store/${application.id}`)
-      )
-    );
-    if (files.includes("form.pdf")) {
-      res.download(
-        path.join(
-          __dirname,
-          `../../../data/pdf_store/${application.id}/form.pdf`
-        )
       );
+
+      if (files.includes("form.pdf")) {
+        res.download(
+          path.join(
+            __dirname,
+            `../../../data/pdf_store/${application.id}/form.pdf`
+          )
+        );
+      }
+    } catch (err) {
+      next(err);
     }
   }
 );
@@ -174,157 +166,164 @@ appRouter.post(
   "/",
   // upload.fields(uploadSchema),
   upload.single("pdf_form"),
-  async (req: any, res: express.Response) => {
+  async (req: any, res: express.Response, next: express.NextFunction) => {
+    const user = res.locals.user;
+
+    // TODO: Add a type for the form schema
     const form = JSON.parse(req.body.meta_data);
-    // console.log(form);
-    const coauthorEmails: string[] = form.coauthors;
-    const supervisorEmails: string[] = form.supervisors;
 
-    const coauthors: Array<User> = [];
-    const supervisors: Array<User> = [];
+    // TODO: Add validation on the backend for email fields (and text fields if needed)
+    const coauthor_emails: string[] = form.coauthors;
+    const supervisor_emails: string[] = form.supervisors;
 
-    coauthorEmails.forEach((coauthorEmail: string) => {
-      User.getByEmail(coauthorEmail).then((user: User) => {
-        if (user) {
-          coauthors.push(user);
-        }
+    try {
+      const coauthor_users = await User.find({
+        where: {
+          email: In(coauthor_emails),
+        },
       });
-    });
 
-    supervisorEmails.forEach((supervisorEmail: string) => {
-      User.getByEmail(supervisorEmail).then((user: User) => {
-        if (user) {
-          supervisors.push(user);
-        }
+      const supervisor_users = await User.find({
+        where: {
+          email: In(supervisor_emails),
+        },
       });
-    });
 
-    const application = new Application({
-      name: form.name,
-      description: form.description,
-      field: form.field,
-      submitter: req.user,
-      // TODO add round robin algorithm for reviewers
-      reviewers: [],
-      coauthors,
-      supervisors,
-    });
+      const applicationRepository: Repository<Application> = getRepository(Application);
 
-    await application.save();
-    if (!application.id) {
-      const re: Response = {
-        status: 500,
-        message: "Failed to save application",
-        data: null,
+      const temp_application: Partial<Application> = {
+        name: form.name,
+        description: form.description,
+        field: form.field,
       };
-      return res.json(re);
-    }
 
-    const defaultReview = new Review({
-      status: ReviewStatus.PENDING,
-      comment: null,
-    });
+      const application = (await applicationRepository.save(
+        temp_application
+      )) as Application;
 
-    await application.addReview(defaultReview);
+      const userApplicationRepository: Repository<UsersApplications> = getRepository(UsersApplications);
 
-    if (application.id) {
-      try {
-        req.file.originalname.replace(" ", "%20");
-        fs.mkdirSync(
-          path.join(__dirname, `../../../data/pdf_store/${application.id}`)
-        );
-        fs.writeFile(
-          path.join(
-            __dirname,
-            `../../../data/pdf_store/${application.id}/form.pdf`
-          ),
-          req.file.buffer,
-          (_err) => {}
-        );
-        const response: Response = {
-          status: 201,
-          message: "Success",
-          data: application,
-        };
-        res.send(JSON.stringify(response));
-      } catch (e) {
-        const response: Response = {
-          status: 500,
-          message: "Internal Server Error",
-          data: null,
-        };
-        console.error("Error writing application form to disk\n", e);
-        res.status(500).send(JSON.stringify(response));
+      for (const coauthor of coauthor_users) {
+        const userApplication = new UsersApplications({
+          application,
+          user: coauthor,
+          role: RoleType.COAUTHOR,
+        });
+
+        await userApplicationRepository.save(userApplication);
       }
+
+      for (const supervisor of supervisor_users) {
+        const userApplication = new UsersApplications({
+          application,
+          user: supervisor,
+          role: RoleType.SUPERVISOR,
+        });
+
+        await userApplicationRepository.save(userApplication);
+      }
+
+      const userApplication = new UsersApplications({
+        application,
+        user,
+        role: RoleType.SUBMITTER,
+      });
+
+      await userApplicationRepository.save(userApplication);
+
+      req.file.originalname.replace(" ", "%20");
+
+      fs.mkdirSync(
+        path.join(__dirname, `../../../data/pdf_store/${application.id}`)
+      );
+
+      fs.writeFile(
+        path.join(
+          __dirname,
+          `../../../data/pdf_store/${application.id}/form.pdf`
+        ),
+        req.file.buffer,
+        (err) => {
+          if (err) throw err;
+        }
+      );
+
+      res.json({
+        status: 201,
+        message: "Successfully created application",
+        data: application,
+      });
+    } catch (err) {
+      next(err);
     }
   }
 );
 
-appRouter.patch("/:id", async (req: express.Request, res: express.Response) => {
-  // console.log(req.body)
-  const body = req.body as Application;
-  const application = await Application.getById(parseInt(req.params.id));
-  if (!application) {
-    return res.status(404).json(sample_404_res);
-  }
-  if (!check_access_app(application, req.user)){
-    return res.status(401).json(sample_401_res);
-  }
-  if (application) {
-    application.name = body.name ? body.name : application.name;
-    application.description = body.description
-      ? body.description
-      : application.description;
-    application.field = body.field ? body.field : application.field;
-    application.supervisors = body.supervisors
-      ? body.supervisors
-      : application.supervisors;
-    application.coauthors = body.coauthors
-      ? body.coauthors
-      : application.coauthors;
-    await application.save();
+appRouter.patch(
+  "/:id",
+  async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    const body: Partial<Application> = req.body;
+    const user = res.locals.user;
+    const applicationId = req.params.id;
 
-    const response: Response = {
-      status: 200,
-      message: "Success",
-      data: application,
-    };
-    res.json(response);
-  } else {
-    const response: Response = {
-      status: 404,
-      message: "Application not found",
-      data: null,
-    };
-    res.json(response);
+    try {
+      const application = await Application.findOne(applicationId);
+
+      if (!application) throw new NotFoundError();
+
+      if (!check_access_app(application, user)) throw new NotAuthorizedError();
+
+      const applicationRepository: Repository<Application> = getRepository(Application);
+
+      // TODO: Test updating an application (specifically adding/removing coauthors)
+      const updatedApplication = await applicationRepository.save({
+        ...application,
+        ...body,
+      });
+
+      res.json({
+        status: 200,
+        message: "Successfully updated application",
+        data: updatedApplication,
+      });
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
 appRouter.delete(
   "/:id",
-  async (req: express.Request, res: express.Response) => {
-    const application = await Application.getById(parseInt(req.params.id));
-    if (!application) {
-      return res.status(404).json(sample_404_res);
-    }
-    if (!check_access_app(application, req.user)){
-      return res.status(401).json(sample_401_res);
-    }
-    if (application) {
-      await application.remove();
-      const response: Response = {
+  async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    const user = res.locals.user;
+    const applicationId = req.params.id;
+
+    try {
+      const application = await Application.findOne(applicationId);
+
+      if (!application) throw new NotFoundError();
+
+      if (!check_access_app(application, user)) throw new NotAuthorizedError();
+
+      const applicationRepository: Repository<Application> = getRepository(Application);
+
+      // TODO: Test that this cascades to the user_application join table
+      await applicationRepository.delete(application.id);
+
+      res.json({
         status: 200,
-        message: "Success",
-        data: null,
-      };
-      res.json(response);
-    } else {
-      const response: Response = {
-        status: 404,
-        message: "Application not found",
-        data: null,
-      };
-      res.json(response);
+        message: "Successfully deleted application",
+      });
+    } catch (err) {
+      next(err);
     }
   }
 );
