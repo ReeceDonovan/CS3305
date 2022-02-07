56ecb9c0cbd8036cf8362ef5a0b38fb7cfa7b6ed
ReeceDonovan <reecedonovan99@gmail.com>
2022-02-07 15:45:33 +0000

    Refactored user router to reflect new schema and middleware
    Signed-off-by: ReeceDonovan <reecedonovan99@gmail.com>

--- api/src/router/user.ts
+++ api/src/router/user.ts
@@ -1,44 +1,83 @@
 import express from "express";
-import User from "../models/user";
-import Response, { sample_401_res, sample_404_res } from "../utils/response";
+import { NotAuthorizedError, NotFoundError } from "src/errors";
+import { protectedRoute } from "src/middleware/protected-route";
+import { QueryDeepPartialEntity } from "typeorm/query-builder/QueryPartialEntity";
+
+import User, { UserType } from "../models/user";
 
 const userRouter = express.Router();
+userRouter.use(protectedRoute);
 
-userRouter.get("/:id", async (req, res) => {
-  let user = await User.getById(parseInt(req.params.id))
-  if (!user) {
-    return res.status(404).json(sample_404_res);
+userRouter.get(
+  "/:id",
+  async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    const uid = req.params.id;
+    const reqUser = res.locals.user;
+
+    try {
+      if (uid !== reqUser.id || reqUser.role !== UserType.COORDINATOR) {
+        throw new NotAuthorizedError();
+      }
+
+      const user = await User.findOne(uid);
+
+      if (!user) {
+        throw new NotFoundError();
+      }
+
+      res.json({
+        status: 200,
+        message: "Successfully fetched user",
+        data: user,
+      });
+    } catch (err) {
+      next(err);
+    }
   }
-  if (req.user != user || req.user.role != "COORDINATOR") {
-    return res.status(401).json(sample_401_res);
+);
+
+userRouter.get(
+  "/",
+  async (
+    _: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    res.json({
+      status: 200,
+      message: "Successfully fetched users",
+      data: res.locals.user,
+    });
+    next();
   }
-  res.json(await User.getById(parseInt(req.params.id)));
-});
+);
 
-userRouter.get("/", (req, res) => {
-  res.send(req.user);
-});
+userRouter.patch(
+  "/",
+  async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    let user = res.locals.user;
+    const body: QueryDeepPartialEntity<User> = req.body;
 
-userRouter.patch("/", (req, res) => {
-  let user = req.user;
-  if (req.body.name){
-    user.name = req.body.name;
+    // TODO: Make a type for the body
+    try {
+      user = await User.update(user.id, body);
+      res.json({
+        status: 200,
+        message: "Successfully updated user",
+        data: user,
+      });
+    } catch (err) {
+      next(err);
+    }
   }
-
-  if (req.body.bio){
-    user.bio = req.body.bio;
-  }
-
-  if (req.body.name){
-    user.school = req.body.school;
-  }
-
-  if (req.body.role){
-    user.role = req.body.role;
-  }
-
-  user.save();
-  res.sendStatus(200);
-});
+);
 
 export default userRouter;
