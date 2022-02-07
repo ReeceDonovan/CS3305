b85fe4b737dad2952594611a5711c57bb63a4621
ReeceDonovan <reecedonovan99@gmail.com>
2022-02-07 15:43:32 +0000

    Refactored login router to reflect new schema and middleware
    Signed-off-by: ReeceDonovan <reecedonovan99@gmail.com>

--- api/src/router/login.ts
+++ api/src/router/login.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 import express from "express";
 import jwt from "jsonwebtoken";
+import { BadRequestError } from "src/errors";
+import { InternalError } from "src/errors/internal-error";
+import { TokenClaims } from "src/middleware/auth-user";
 
 import config from "../config/config";
 import User from "../models/user";
@@ -17,37 +20,45 @@ loginRouter.get("/", async (_req: express.Request, res: express.Response) => {
 
 loginRouter.get(
   "/callback",
-  async (req: express.Request, res: express.Response) => {
+  async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
     const code = req.query["code"];
-    const r = await axios.post("https://oauth2.googleapis.com/token", {
-      code,
-      client_id: config.get().oauthConfig.oauthClientId,
-      client_secret: config.get().oauthConfig.oauthClientSecret,
-      grant_type: "authorization_code",
-      redirect_uri: `${config.get().apiURL}/login/callback`,
-    });
 
-    const access_token = r.data.access_token;
-    const resp = await axios(
-      `https://www.googleapis.com/oauth2/v2/userinfo?access_token=${access_token}`
-    );
+    try {
+      const r = await axios.post("https://oauth2.googleapis.com/token", {
+        code,
+        client_id: config.get().oauthConfig.oauthClientId,
+        client_secret: config.get().oauthConfig.oauthClientSecret,
+        grant_type: "authorization_code",
+        redirect_uri: `${config.get().apiURL}/login/callback`,
+      });
 
-    const mailDomain = resp.data.email.split("@")[1];
+      const access_token = r.data.access_token;
+      const resp = await axios(
+        `https://www.googleapis.com/oauth2/v2/userinfo?access_token=${access_token}`
+      );
 
-    if (
-      config.get().oauthConfig.allowedDomains.includes(mailDomain) &&
-      resp.data.verified_email === true
-    ) {
+      const mailDomain = resp.data.email.split("@")[1];
+
+      if (
+        resp.data.verified_email !== true ||
+        !config.get().oauthConfig.allowedDomains.includes(mailDomain)
+      ) {
+        throw new BadRequestError(
+          "Email not verified, or not from allowed domain"
+        );
+      }
+
       const email = resp.data.email;
       const avatar = resp.data.picture;
 
-      let user = await User.getByEmail(email);
+      let user = await User.findOne({ email });
 
-      if (user === undefined) {
-        user = new User({
-          email: email,
-          avatar: avatar,
-        });
+      if (!user) {
+        user = new User({ email, avatar });
         await user.save();
       }
 
@@ -57,14 +68,16 @@ loginRouter.get(
           exp: Math.floor(Date.now() / 1000) + 6 * 60 * 60,
         },
         config.get().signingKey,
-        (err: Error, token: string) => {
+        (err: Error, token: TokenClaims) => {
           if (err) {
-            console.error(err);
-            res.status(500).send("Error signing token");
+            throw new InternalError("Error signing token");
+          } else {
+            res.redirect(`${config.get().uiURL}/login?token=${token}`);
           }
-          res.redirect(`http://localhost:3000/login?token=${token}`);
         }
       );
+    } catch (err) {
+      next(err);
     }
   }
 );
