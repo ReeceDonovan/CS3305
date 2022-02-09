676af417fd766a676e9807bce0ff22c298f12d1c
Thomas Daniel Galligan <thomas007g@gmail.com>
2022-02-09 14:59:51 +0000

    added structure for reviewer suggestion

--- api/src/router/user.ts
+++ api/src/router/user.ts
@@ -2,11 +2,13 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import express from "express";
+import { getRepository, Repository } from "typeorm";
 import { QueryDeepPartialEntity } from "typeorm/query-builder/QueryPartialEntity";
 
 import { NotAuthorizedError, NotFoundError } from "../errors";
 import { protectedRoute } from "../middleware/protected-route";
 import User, { UserType } from "../models/user";
+import UsersApplications from "../models/usersApplications";
 
 const userRouter = express.Router();
 userRouter.use(protectedRoute);
@@ -73,6 +75,45 @@ userRouter.patch(
         message: "Successfully updated user",
         data: updatedUser,
       });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+userRouter.get(
+  "/reviewers",
+  async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    const user = res.locals.user;
+    const target = req.query.t;
+
+    if (user.role !== UserType.COORDINATOR) throw new NotAuthorizedError();
+
+    try {
+      const reviewers = await User.find({
+        where: {
+          role: UserType.REVIEWER,
+        },
+      });
+
+      if (!reviewers) throw new NotFoundError();
+
+      switch (target) {
+        case "suggest":
+          // FIXME add suggestion algo
+          break;
+
+        default:
+          res.json({
+            status: 200,
+            message: "Successfully fetched reviewers",
+            data: reviewers,
+          });
+      }
     } catch (err) {
       next(err);
     }
