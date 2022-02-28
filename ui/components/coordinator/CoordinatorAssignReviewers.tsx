3a029ad5a06fd41d851ae38facdd592b93591bcc
Thomas Daniel Galligan <thomas007g@gmail.com>
2022-02-28 13:10:21 +0000

    changed reviewer combobox to render avatar, changed request body type

--- ui/components/coordinator/CoordinatorAssignReviewers.tsx
+++ ui/components/coordinator/CoordinatorAssignReviewers.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { Button, ComboBox, InlineLoading, Tag } from "carbon-components-react";
 import React, { useContext, useEffect, useState } from "react";
 import { useRouter } from "next/router";
@@ -9,79 +10,100 @@ export default function CoordinatorAssignReviewers() {
   const nm_ctx = useContext(NetworkManagerContext);
 
   const [loading, setLoading] = useState<boolean>(true);
-  const [submitting, setSubmitting] = useState<boolean>(false);
 
   const [comboReviewers, setComboReviewers] = useState<any[]>([]);
   const [tagReviewers, setTagReviewers] = useState<any[]>([]);
 
-  useEffect(()=> {
+  const [reviewerIds, setReviewerIds] = useState<any[]>([]);
+
+  useEffect(() => {
     (async () => {
-      const [res, err_code] = await nm_ctx.request({
-        method: "GET",
-        path: `/users/reviewers`,
-      });
-      let reviewerEmails = [];
-      if (res.data) {
-        for (let i = 0; i < res.data.length; i++) {
-          reviewerEmails.push(res.data[i].email);
+      if (loading) {
+        const [res] = await nm_ctx.request({
+          method: "GET",
+          path: `/users/reviewers`,
+        });
+        if (res.data) {
+          setComboReviewers(res.data);
         }
+        setLoading(false);
       }
-      setComboReviewers(reviewerEmails);
-      setLoading(false);
-  })()}, []);
+    })();
+  }, [nm_ctx, loading]);
 
   return (
     <>
-      {!loading? 
-      <div className={styles.container}>
-        <div className={styles.dropdown}>
-          <ComboBox id={""} placeholder={"Assign Reviewer"} items={comboReviewers} onChange={(e) => {
-            if (e.selectedItem) {
-              comboReviewers.splice(comboReviewers.indexOf(e.selectedItem), 1);
-              setComboReviewers([...comboReviewers]);
-              setTagReviewers([...tagReviewers, e.selectedItem]);
-            }
-          }}/>
-        </div>
-        <div className={styles.listReviewers}>
-          {tagReviewers.map((elem, i) => (
-            <Tag
-              key={i}
-              onClick={(e) => {
-                e.preventDefault();
-                const t = tagReviewers.splice(i, 1);
-                setTagReviewers([...tagReviewers]);
-                setComboReviewers([...comboReviewers, t[0]])
+      {!loading ? (
+        <div className={styles.container}>
+          <div className={styles.dropdown}>
+            <ComboBox
+              id={""}
+              placeholder={"Assign Reviewer"}
+              items={comboReviewers}
+              // itemToString={(item) => (item ? item.email : "No data")}
+              itemToElement={(item) => (
+                <span
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                  }}
+                >
+                  <img
+                    alt={item.email + "'s avatar"}
+                    src={item?.avatar}
+                    width="20px"
+                    height="20px"
+                    style={{ borderRadius: "50%", marginRight: "1rem" }}
+                  />
+                  <p>{item.email}</p>
+                </span>
+              )}
+              onChange={(e) => {
+                if (e.selectedItem) {
+                  comboReviewers.splice(
+                    comboReviewers.indexOf(e.selectedItem.email),
+                    1
+                  );
+                  setComboReviewers([...comboReviewers]);
+                  setTagReviewers([...tagReviewers, e.selectedItem.email]);
+                  setReviewerIds([...reviewerIds, { id: e.selectedItem.id }]);
+                }
+              }}
+            />
+          </div>
+          <div className={styles.listReviewers}>
+            {tagReviewers.map((elem, i) => (
+              <Tag
+                key={i}
+                onClick={(e) => {
+                  e.preventDefault();
+                  const t = tagReviewers.splice(i, 1);
+                  setTagReviewers([...tagReviewers]);
+                  setComboReviewers([...comboReviewers, t[0]]);
+                  setReviewerIds([...reviewerIds.splice(i, 1)]);
+                }}
+              >
+                {elem}
+              </Tag>
+            ))}
+          </div>
+          <div className={styles.assignBtn}>
+            <Button
+              onClick={() => {
+                nm_ctx.request({
+                  method: "PUT",
+                  path: `/applications/${router.query.slug}/reviewers`,
+                  data: [...reviewerIds],
+                });
               }}
             >
-              {elem}
-            </Tag>
-          ))}
+              Assign
+            </Button>
+          </div>
         </div>
-        <div className={styles.assignBtn}>
-          <Button
-            onClick={() => {
-              setSubmitting(true);
-              nm_ctx.request({
-                method: "PUT",
-                path: `/applications/${router.query.slug}/reviewers`,
-                data: {tagReviewers}
-              });
-              nm_ctx.request({
-                method: "PATCH",
-                path: `/applications/${router.query.slug}`,
-                data: { app_status: "REVIEW" },
-                show_progress: true,
-              });
-              setSubmitting(false);
-            }}
-            disabled={submitting}
-          >
-            Assign
-          </Button>
-        </div>
-      </div>
-      : <InlineLoading />}
+      ) : (
+        <InlineLoading />
+      )}
     </>
   );
 }
