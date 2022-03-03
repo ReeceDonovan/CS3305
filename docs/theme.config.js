1fda5df865d2d4d8b8b96a0179fa58926dd36e7b
Thomas Daniel Galligan <thomas007g@gmail.com>
2022-03-03 16:20:25 +0000

    added docs website

---
+++ docs/theme.config.js
@@ -0,0 +1,24 @@
+export default {
+  repository: 'https://github.com/reecedonovan/cs3305', // project repo
+  docsRepository: 'https://github.com/ReeceDonovan/CS3305/tree/main/docs', // docs repo
+  branch: 'master', // branch of docs
+  path: '/', // path of docs
+  titleSuffix: ' - SREC Docs',
+  nextLinks: true,
+  prevLinks: true,
+  search: true,
+  customSearch: null, // customizable, you can use algolia for example
+//   darkMode: true,
+  footer: true,
+  footerText: `MIT ${new Date().getFullYear()} © Team 1 CS3305`,
+  footerEditOnGitHubLink: false,
+  logo: <>
+    <svg>...</svg>
+    <span>SREC Website Documentation</span>
+  </>,
+  head: <>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Documentation for the UCC SREC website" />
+    <meta name="og:title" content="SREC Website Documentation" />
+  </>
+}
